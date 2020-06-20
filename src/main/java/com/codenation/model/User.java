15536c1cf45284d1b5d26e3a08c31dc02e33bf79
Lucas Laet <50890960+llaet@users.noreply.github.com>
2020-06-20 03:25:19 -0300

    lombok constructor annotations removed

--- src/main/java/com/codenation/model/User.java
+++ src/main/java/com/codenation/model/User.java
@@ -24,14 +24,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
 @Table(name = "users", uniqueConstraints =  @UniqueConstraint(columnNames = "email"))
 @EntityListeners(AuditingEntityListener.class)
 public class User implements UserDetails {
